c0a0142cb67c8beaa21aa93b719c2fe04ed6353a
Nishio <l.nishio@servorobot.com>
2020-11-09 17:25:21 -0500

    update main

--- P10_00_00 Tests/P10_00_00-Main/P10_00_00-Main.ino
+++ P10_00_00 Tests/P10_00_00-Main/P10_00_00-Main.ino
@@ -1,137 +1,116 @@
+/*
+   P10_00_00-Main
+   Project: P10_00_00 NotMildewPls
+   Schematic: P10_00_00-Main.fzz
+   Description: Arduino sketch for P10_00_00 NotMildewPls
+*/
+
+//Includes
 #include <tinysnore.h>
 #include <EEPROM.h>
 
 //Variables
-long TimeStart;
-long TimeOnMs;
-long TimeOffMs;
-int address = 0; //Address for where to store PercentOn settings
-int led = 2; //2
-int button = 1;
-int fan = 0;
+long TimeStart;    //Timer variable
+long TimeOnMs;     //Amount of time to stay on
+long TimeOffMs;    //Amount of time to stay off
+int address = 0;   //Address for where to store PercentOn settings
+int led = 2;       //Pin connected to the led
+int button = 1;    //Pin connected to the button
+int fan = 0;       //Pin connected to TIP120 base pin
 
 //Settings
-int Debounce = 200;
-byte HoldTime = 2000;
-byte PercentOn = 25;
-long StartupDelay = 60000;
-int Period = 3600; //In seconds 3600
+int Debounce = 200;         //Delay time to ignore button bounce
+byte HoldTime = 2000;       //How long to hold to be counted as a long press
+byte PercentOn = 25;        //Initial time on 25%
+long StartupDelay = 60000;  //Amount of time to wait for modification of settings at startup
+int Period = 3600;          //One period is 1 hr 
 
 
-// the setup function runs once when you press reset or power the board
+//Code that gets run during boot up
 void setup() {
   //Set pin states
   pinMode(led, OUTPUT);
   pinMode(fan, OUTPUT);
+  pinMode(button, INPUT);
 
-  //Start up the program
-  //Serial.begin(9600);
-  FlashSetShort();
+  //Flash to indicate that boot sequence
+  BlinkFast(2);
   delay(1000);
-  FlashSetShort();
+  BlinkFast(2);
 
-  //Check for stored PercentOn
-  if (EEPROM.read(address) == 255)
+  //Check for stored Settings
+  if (EEPROM.read(address) == 255) //No settings saved
   {
     EEPROM.write(address, PercentOn);
     Blink(7);
   }
-  else
+  else //Load the settings
   {
     PercentOn = EEPROM.read(address);
     Blink(4);
   }
 
-  //Wait 5 min to search for a menu request
+  //Wait to search for a menu request
   long Start = millis();
   while((millis()-Start) < StartupDelay)
   {
-    //Led on to indicate that it is availible for programming
+    //Led on to indicate that it is available for programming
     digitalWrite(led,HIGH);
   
-    //Check for Menu request
+    //Check for Menu request (long press)
     if(PressType() == 2)
     {
+    //Display programming request made
       digitalWrite(led,LOW);
       delay(1000);
+    
+    //Get, save, and display the new settings
       PercentOn  = SetTimeOn();
-      //Serial.println("TimeSet");
-      //Serial.print("PercentOn: ");
-      //Serial.println(PercentOn);
-      //Serial.print("TimeOnMs: ");
-      //Serial.println(TimeOnMs);
       EEPROM.write(address, PercentOn);
       BinkPercent();
       delay(1000);
     }
   }
+  
+  //Turn off led to indicate that settings cannot be written
   digitalWrite(led,LOW);
   delay(1000);
   
   //Calculate time on in ms
-  //TimeOnMs = 10L * Period * PercentOn;
-  TimeOnMs  = 900000;
+  TimeOnMs = 10L * Period * PercentOn;
   TimeOffMs = 1000L * Period - TimeOnMs;
-  //Serial.print("Settings: ");
-  //Serial.print(TimeOnMs);
-  //Serial.print(",");
-  //Serial.print(TimeOffMs);
   
 }
 
-// the loop function runs over and over again forever
+//Main program loop
 void loop() {
-  digitalWrite(fan,HIGH);
-  delay(3000);
-  digitalWrite(fan,LOW);
-  BinkPercent();
-  delay(1000);
-  BinkTime(TimeOnMs);
-  delay(1000);
-  BinkTime(TimeOffMs);
-  delay(1000);
-  //Serial.println("On");
+  //Turn on the fan and led
   digitalWrite(led,HIGH);
   digitalWrite(fan,HIGH);
   delay(TimeOnMs);
-  //Serial.println("On");
+  //Turn off the fan and led
   digitalWrite(fan,LOW);
   digitalWrite(led,LOW);
-  //delay(TimeOffMs);
+  
+  //Go to sleep until next cycle
   snore(TimeOffMs);
 }
-void FlashSet()
-{
-  for (int i = 0; i <= 3; i++) 
-  {
-    digitalWrite(led,HIGH);
-    delay(100);
-    digitalWrite(led,LOW);
-    delay(100);
-  }
-  delay(1000);
-}
-void FlashSetShort()
-{
-  for (int i = 0; i <= 2; i++) 
-  {
-    digitalWrite(led,HIGH);
-    delay(100);
-    digitalWrite(led,LOW);
-    delay(100);
-  }
-  delay(1000);
-}
+
+//Program that determines the type of input received 0=none 1=short 2=long 
 int PressType()
 {
+  //Default 0 is button not pressed
   int Result = 0;
+  
   //Check for button press
   if(digitalRead(button) == LOW)
   {
     Result = 1;
     TimeStart = millis();
     delay(Debounce);
-    while(digitalRead(button) == LOW)
+    
+  //Check for press and hold
+  while(digitalRead(button) == LOW)
     {
       long Delta = millis() - TimeStart;
       if (Delta > HoldTime)
@@ -143,6 +122,113 @@ int PressType()
   }
   return Result;
 }
+
+//Menu interface for setting the percent on value
+/*
+ * Flashes quickly 3 times to indicate that it's ready to accept values
+ * Each short press is counted as +1 for the tens value 
+ * (max value is 9)
+ * (10 second time out if no input given)
+ * Long press will set the tens value
+ * Will blink quickly 2 times to indicate that the tens value has been set
+ * Each short press is counted as +1 for the ones value
+ * (max value is 9)
+ * (10 second time out if no input given)
+ * Long press will set the ones value
+ * Will blink quickly 2 times to indicate that the ones value has been set
+ */
+byte SetTimeOn()
+{
+  //Display now in set settings mode
+  BlinkFast(3);
+
+  //Set variables
+  byte Tens = 0;
+  byte Ones = 0;
+  bool Set = false;
+  byte Result = 0;
+  long LastInput = millis();
+
+  //Get the 10X factor
+  while(!Set)
+  {
+    //Get button press
+    int Press = PressType();
+    
+  //Short press increase the count
+    if(Press == 1)
+    {
+      LastInput = millis();
+      Tens += 1;
+    }
+  
+    //Max count is 9 or break at long press
+    if((Press == 2) || (Tens == 9))
+    {
+      Set = true;
+    }
+  
+    //Check for no input
+    if((millis() - LastInput) > 10000)
+    {
+      return PercentOn;
+    }
+  }
+  //Indicate that the value has been set
+  BlinkFast(2);
+  
+  //Get the 1X factor
+  Set = false;
+  LastInput = millis();
+  while(!Set)
+  {
+    //Get button press
+    int Press = PressType();
+  
+    //Short press increase the count
+    if(Press == 1)
+    {
+      LastInput = millis();
+      Ones += 1;
+    }
+  
+    //Max count is 9 or break at long press
+    if((Press == 2) || (Ones == 9))
+    {
+      Set = true;
+    }
+  
+    //Check for no input
+    if((millis() - LastInput) > 10000)
+    {
+      return PercentOn;
+    }
+  }
+  
+  //Indicate that the value has been set
+  BlinkFast(2);
+  
+  //Calculate and return the result
+  Result = Tens*10 + Ones;
+  return Result; 
+}
+
+
+//Bink quickly given number of times
+void BlinkFast(int Ct)
+{
+  for (int i = 0; i < Ct; i++) 
+  {
+    digitalWrite(led,HIGH);
+    delay(100);
+    digitalWrite(led,LOW);
+    delay(100);
+  }
+  delay(1000);
+}
+
+
+//Blink slowly a given number of times
 void Blink(int Ct)
 {
   for (int i = 0; i < Ct; i++) 
@@ -153,127 +239,13 @@ void Blink(int Ct)
     delay(500);
   }
 }
-byte SetTimeOn()
-{
-  byte Result = 0;
-  long LastInput = millis();
- 
-  //Display now in set flash mode
-  FlashSet();
 
-  //Set variables
-  byte Tens = 0;
-  byte Ones = 0;
-  bool Set = false;
-
-  //Get the 10X factor
-  while(!Set)
-  {
-    //Get button press
-    int Press = PressType();
-    //Short press increase the count
-    if(Press == 1)
-    {
-      LastInput = millis();
-      Tens += 1;
-    }
-    //Max count is 9 or brreak at long press
-    if((Press == 2) || (Tens == 9))
-    {
-      Set = true;
-    }
-    //Check for no input
-    if((millis() - LastInput) > 10000)
-    {
-      return PercentOn;
-    }
-    //Serial.print("Tens: ");
-    //Serial.println(Tens);
-  }
-  //Indicate that the value has been set
-  FlashSetShort();
-  //Get the 1X factor
-  Set = false;
-  LastInput = millis();
-  while(!Set)
-  {
-    //Get button press
-    int Press = PressType();
-    //Short press increase the count
-    if(Press == 1)
-    {
-      LastInput = millis();
-      Ones += 1;
-    }
-    //Max count is 9 or brreak at long press
-    if((Press == 2) || (Ones == 9))
-    {
-      Set = true;
-    }
-    //Check for no input
-    if((millis() - LastInput) > 10000)
-    {
-      return PercentOn;
-    }
-  //Serial.print("Ones: ");
-    //Serial.println(Ones);
-  }
-  //Indicate that the value has been set
-  FlashSetShort();
-  //Display the 10X factor and the 1X factor
-  Blink(Tens);
-  FlashSetShort();
-  Blink(Ones);
-  Result = Tens*10 + Ones;
-  //Serial.print("Result: ");
-  //Serial.println(Result);
-  return Result; 
-}
+//Blink out the PercentOn value (tens)(3x fast blink)(ones) 
 void BinkPercent()
 {
   byte Tens = PercentOn/10;
   byte Ones = PercentOn - Tens*10;
   Blink(Tens);
-  FlashSetShort();
+  BlinkFast(3);
   Blink(Ones);
-}
-void BinkTime(long Time)
-{
-  byte Val1 = Time/1000000;
-  Time = Time - (1000000*Val1);
-  byte Val2 = Time/100000;
-  Time = Time - (100000*Val2);
-  byte Val3 = Time/10000;
-  Time = Time - (10000*Val3);
-  byte Val4 = Time/1000;
-  Time = Time - (1000*Val4);
-  byte Val5 = Time/100;
-  Time = Time - (100*Val5);
-  byte Val6 = Time/10;
-  Time = Time - (10*Val6);
-  byte Val7 = Time/1;
-  Time = Time - (1*Val7);
-  FlashSetShort();
-  Blink(Val1);
-  delay(500);
-  FlashSetShort();
-  Blink(Val2);
-  delay(500);
-  FlashSetShort();
-  Blink(Val3);
-  delay(500);
-  FlashSetShort();
-  Blink(Val4);
-  delay(500);
-  FlashSetShort();
-  Blink(Val5);
-  delay(500);
-  FlashSetShort();
-  Blink(Val6);
-  delay(500);
-  FlashSetShort();
-  Blink(Val7);
-  delay(500);
-  FlashSetShort();
-  delay(1000);
 }
